97a606a8a07eaebe65ed65d28817a39333d8d030
loouisbrandon <brandonlouisferreira@gmail.com>
2022-01-18 18:10:37 -0300

    saved 3

--- components/Body.js
+++ components/Body.js
@@ -3,10 +3,15 @@ import Search from "./Search";
 
 function Body() {
     const [search, setSearch] = useState("");
+    const [ searchResults, setSearchResults] = useState([]);
     return (
         <section className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow
         md:mr-2.5">
             <Search search={search} setSearch={setSearch}/>
+            <div className="grid overflow-y-scroll scrollbar-hide h-96 py-4 grid-cols-2 lg:grid-cols-3 
+            xk:grid-cols-4 gap-x-4 gap-y-8 p-4">
+                
+            </div>
         </section>
     )
 }
---
+++ pages/auth/signin.js
@@ -0,0 +1,9 @@
+function Signin() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Signin
