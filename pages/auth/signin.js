9f74086aefbee352bfdba08bf29625be50891f29
loouisbrandon <brandonlouisferreira@gmail.com>
2022-01-19 22:05:05 -0300

    saved 4

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,9 +1,42 @@
-function Signin() {
+import { getProviders, useSession } from "next-auth/react";
+import Head from "next/head";
+import Image from "next/image";
+
+function Signin({providers}) {
+    const {data: session} = useSession()
     return (
         <div>
+           <Head>
+               <title>Login - Spotify</title>
+               <link rel="icon" href="/favicon.ico" />
+               </Head> 
             
+               <Image
+        src="https://rb.gy/y9mwtb"
+        height={250}
+        width={600}
+        objectFit="contain"
+        className="animate-pulse"
+      />
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="text-white py-4 px-6 rounded-full bg-[#1db954] transition duration-300 ease-out border border-transparent uppercase font-bold text-xs md:text-base tracking-wider hover:scale-105 hover:bg-[#0db146]"
+            onClick={() => signIn(provider.id)}
+          >
+            Sign in with {provider.name}
+          </button>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default Signin
+
+export async function getServerSideProps() {
+    const providers = await getProviders();
+    return { 
+        props: { providers },
+    };
+}
